93f200385dca3754f58f68e65a14e1a0c15682a6
Bharat Panchal <bharatpanchal3578@gmail.com>
2025-10-16 18:17:37 +0530

    Started the development server and Refactor branch: baseline before API restructure

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import os
 from app import create_app
 
-config_path = os.getenv("FLASK_CONFIG", "app.config.ProductionConfig")
+config_path = os.getenv( "FLASK_CONFIG", "app.config.DevelopmentConfig")
 app = create_app(config_path)
 
 if __name__ == "__main__":
