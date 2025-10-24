ee9567365abbee5a43f68ea9ca011340a4e4b528
Bharat Panchal <bharatpanchal3578@gmail.com>
2025-10-24 10:32:57 +0530

    Testing home route by checking status code and some content part of our html template response

---
+++ tests/test_routes/test_home.py
@@ -0,0 +1,22 @@
+import pytest
+from flask import url_for
+
+def test_home_page(client):
+    """
+    Test the home page route.
+    Check if the status code is 200 and page content is correct.
+    """
+    response = client.get(url_for('home.home'))
+
+    assert response.status_code == 200
+
+def test_home_page_content(client):
+    """
+    Test the home page content.
+    Check if the response data contains some home page content by testing some text from home page"
+    """
+    response = client.get(url_for('home.home'))
+    content = response.get_data(as_text=True)
+
+    assert "Welcome to Task Manager" in content
+    
